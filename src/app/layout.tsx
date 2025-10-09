cc3606b807a32fcfd11cc57ad13094d11ded1da0
Brian Whaley <brian.whaley@gmail.com>
2025-10-09 10:54:23 -0400

    footer fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	// const [ host, setHost ] = useState<string | null>(null);
 	
 	// useEffect(() => {
-	CalloutAnimate();
+	// if (typeof window !== "undefined"){ 
+    	CalloutAnimate();
+	// }
 	// }, []);
 
 	useEffect(() => {
